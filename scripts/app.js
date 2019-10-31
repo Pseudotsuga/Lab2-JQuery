fc09d5053c8da138e84bd658b805bed966592bad
Conor <conormccue@gmail.com>
2019-10-31 10:30:18 -0700

    finish feature 2 templating

--- scripts/app.js
+++ scripts/app.js
@@ -13,32 +13,21 @@ function Image (image){
 Image.all = [];
 
 Image.readJson = () => {
-  $.get('../data/page-1.json')
-    .then( data => {
-      data.forEach( thing => {
-        Image.all.push(new Image(thing));
-      });
-    })
+  let data = null;
+  window.location.pathname === '/index.html' ? data = $.get('../data/page-1.json')
+  : data = $.get('../data/page-2.json');
+  data.then( item => {
+    item.forEach( thing => {
+      Image.all.push(new Image(thing));
+    });
+  })
     .then(Image.loadImages);
 };
 
 Image.prototype.render = function(){
-//   //Create
-//   let imageClone = $('#photo-template').clone();
-//   let $imageClone = $(imageClone[0].content);
-//   //Give Content
-//   $imageClone.find('h2').text(this.title);
-//   $imageClone.find('img').attr('src', this.image_url);
-//   $imageClone.find('img').attr('alt', this.description);
-//   $imageClone.find('p').text(this.description);
-//   $imageClone.removeClass('clone');
-//   $imageClone.find('section').addClass(`content ${this.keyword}`);
-//   //Render
-//   $imageClone.appendTo('main');
-let source = $('#photo-template').html();
-let template = Handlebars.compile(source);
-return template(this);
-
+  let source = $('#photo-template').html();
+  let template = Handlebars.compile(source);
+  return template(this);
 };
 
 Image.prototype.renderOptions = function(){
@@ -61,7 +50,6 @@ Image.loadImages = () => {
     $('main').append(image.render());
     image.renderOptions();
   });
-  $('.content').hide();
   $('select').on('change', selectEvent);
 };
 
